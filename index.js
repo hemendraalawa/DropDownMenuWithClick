a44932d855575ae1120b28e189db9fcf4199c554
Hemendra Alawa <hemendraalawa2017@gmail.com>
2024-08-19 01:15:00 +0530

    Clickable Dropdown With Animation Uploaded
    18-10-2023

---
+++ index.js
@@ -0,0 +1,12 @@
+const dropdown = document.querySelector(".dropdown");
+
+dropdown.addEventListener("click",()=>{
+    dropdown.classList.toggle("active");
+});
+
+// dropdown.addEventListener("mouseleave",()=>{
+//     dropdown.classList.toggle("active"); 
+// });
+
+
+
